5a0fb166872e8c484448c01330ba89ebbd7a15fb
Nikola Tadic <ntadic4419rn@raf.edu.rs>
2021-12-12 12:20:46 +0100

    added function choose_texts

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ specialCharacters = ['=', '-', ',', '!', '?', '.', '$', '(', ')', '[', ']', ';',
 
 #'''Dohvata naslove zahtevanog broja stranica koje se pojavaljuju kao
 #  rezultati pretrage za zadatu kljucnu rec'''
-def get_pages(query, results=5): #staviti posle results = 50
+def get_pages(query, results=10): #staviti posle results = 50
   titles = wikipedia.search(query, results=results)
   pages = list()
   for title in titles:
@@ -37,38 +37,50 @@ def add_terminator(array, currentChar):
         else:
             return array + list(currentChar)
 
-def reduce_text(array, currentChar):
-    #if(len(array) >= 5 and currentChar == "$"): # umesto 5 staviti 10_000 posle
-        #ovde sad nekako treba da osiguram da se ova funkcija vise ne poziva, tj. da reduce prekine sa radom. KAKO???
-    return array + list(currentChar)
+def reduce_text(tuple, currentChar):
+    array, flag = tuple
+    if flag :
+        return (array, flag)
+    if len(array) >= 10_000 and currentChar == "$":
+        flag = True
+    return (array + list(currentChar), flag)
 
 
-def prepare():
-    string1 = "a aa!bbb! DA  DAd da $adsad) d (s" \
-             "da sdS)"
-    res = map(lower_case, string1)
+def prepare(tuple):
+    query, string1 = tuple
+    res = map(lower_case, str(string1))
     string2 = reduce(remove_special_chars, list(res), [])
     string3 = reduce(add_terminator, string2, []) #ukoliko imamo u stringu vise povezanih space-eva, stavice za svaki space $, da li je to validno?
     string3.append("$")
+    string4, boolean = reduce(reduce_text, string3, ([], False))
+    return query, string4
 
-    string4 = reduce(reduce_text, string3[0:5], [])#umesto 5 staviti 10_000 posle; da li postoji nacin da se ogranici reduce tj. da se break-uje kada se ispuni neki uslov?
-
-    print(string1)
-    print(string2)
-    print(string3)
-    print(string4)
-
+def choose_texts(tuple, currentElement): #uzima po pet tekstova za svaku kljucnu rec
+    newArray, text, index, counter = tuple
+    key, currentChar = currentElement
+    previousKey, value = text[index-1]
+    if key != previousKey:
+        counter = 0
+    if counter < 5:
+        return (newArray + list(currentChar), text, index+1, counter+1)
+    else:
+        return (newArray, text, index+1, counter+1)
 if __name__ == '__main__':
-    #keywords = ['Beograd', 'Prvi svetski rat', 'Protein', 'Mikroprocesor', 'Stefan Nemanja', 'Košarka']
-    #pool = multiprocessing.pool.ThreadPool(multiprocessing.cpu_count())
-    #titlesMatrix = pool.map(get_pages, keywords, 5) #titlesMatrix = map(get_pages, keywords)-> za 2 sekunde sporije od map-a iz pool-a
-    #titlesList = reduce(append_list, titlesMatrix, [])
+    keywords = ['Beograd', 'Prvi svetski rat', 'Protein', 'Mikroprocesor', 'Stefan Nemanja', 'Košarka']
+    pool = multiprocessing.pool.ThreadPool(multiprocessing.cpu_count())
+    titlesMatrix = pool.map(get_pages, keywords) #titlesMatrix = map(get_pages, keywords)-> za 2 sekunde sporije od map-a iz pool-a
+    titlesList = reduce(append_list, titlesMatrix, [])
+
     #print(titlesList)
-    prepare()
+    processedText = pool.map(prepare, titlesList)
 
-#stao sam kod drugog zadatka, tacka 4.
+    print(processedText)
+
+    chosenTexts, oldText, endIndex, cnt = reduce(choose_texts, processedText, ([], processedText, 0, 0))
+    print(chosenTexts)
 
 
+# stao sam kod zadatka 3, druga tacka
 
 
 
