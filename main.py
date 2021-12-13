53aca18deadfb27e4de82a3f13e5c85ac8e6ee78
Nikola Tadic <ntadic4419rn@raf.edu.rs>
2021-12-13 17:54:34 +0100

    added function insert_token and for loop

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ specialCharacters = ['=', '-', ',', '!', '?', '.', '$', '(', ')', '[', ']', ';',
 
 #'''Dohvata naslove zahtevanog broja stranica koje se pojavaljuju kao
 #  rezultati pretrage za zadatu kljucnu rec'''
-def get_pages(query, results=50): #staviti posle results = 50
+def get_pages(query, results=10): #staviti posle results = 50
   titles = wikipedia.search(query, results=results)
   pages = list()
   for title in titles:
@@ -37,8 +37,8 @@ def add_terminator(array, currentChar):
         else:
             return array + list(currentChar)
 
-def reduce_text(tuple, currentChar):
-    array, flag = tuple
+def reduce_text(args, currentChar):
+    array, flag = args
     if flag :
         return (array, flag)
     if len(array) >= 10_000 and currentChar == "$":
@@ -46,8 +46,8 @@ def reduce_text(tuple, currentChar):
     return (array + list(currentChar), flag)
 
 
-def prepare(tuple):
-    query, string1 = tuple
+def prepare(args):
+    query, string1 = args
     res = map(lower_case, str(string1))
     string2 = reduce(remove_special_chars, list(res), [])
     string3 = reduce(add_terminator, string2, []) #ukoliko imamo u stringu vise povezanih space-eva, stavice za svaki space $, da li je to validno?
@@ -55,8 +55,8 @@ def prepare(tuple):
     string4, boolean = reduce(reduce_text, string3, ([], False))
     return query, string4
 
-def choose_texts(tuple, currentElement): #uzima po pet tekstova za svaku kljucnu rec
-    newArray, text, index, counter = tuple
+def choose_texts(args, currentElement): #uzima po pet tekstova za svaku kljucnu rec; za sada ne bira nasumicno, nego za svaku kljucnu rec prvih 5 tekstova!
+    newArray, text, index, counter = args
     key, currentChar = currentElement
     previousKey, value = text[index-1]
     if key != previousKey:
@@ -66,8 +66,8 @@ def choose_texts(tuple, currentElement): #uzima po pet tekstova za svaku kljucnu
     else:
         return (newArray, text, index+1, counter+1)
 
-def create_tokens(tuple, currentChar):
-    array, chosenTexts, index = tuple
+def create_tokens(args, currentChar):
+    array, chosenTexts, index = args
     if index == len(chosenTexts)-1 or currentChar == "$":
         return (array, chosenTexts, index+1)
     token = currentChar + chosenTexts[index+1]
@@ -92,26 +92,54 @@ def reduce_token_tuples(args, currentTokenTuple):
         copiedArray[index] = (key, int(num)+1)
         return (copiedArray, token, index)
 
+def insert_token(args, currentString):
+    array, tokenTuple, tmp, buffer = args
+    token, number = tokenTuple
+    flag = False
+    if (currentString in token): # a,g,e; currentString = ag
+        flag = True
+        tmp += currentString
+    if flag == False:
+        array += buffer
+    helpArray = []
+    if tmp == token:
+        helpArray.append(token)
+        return (array + helpArray, tokenTuple, "", [])
+    helpArray.append(currentString)
+    if (flag):
+        buffer += helpArray
+        return (array, tokenTuple, tmp, buffer)
+    else:
+        return (array + helpArray, tokenTuple, "", [])
 if __name__ == '__main__':
     keywords = ['Beograd', 'Prvi svetski rat', 'Protein', 'Mikroprocesor', 'Stefan Nemanja', 'Košarka']
     pool = multiprocessing.pool.ThreadPool(multiprocessing.cpu_count())
     titlesMatrix = pool.map(get_pages, keywords) #titlesMatrix = map(get_pages, keywords)-> za 2 sekunde sporije od map-a iz pool-a
     titlesList = reduce(append_list, titlesMatrix, [])
     processedText = pool.map(prepare, titlesList)
-    chosenTexts, oldText, endIndex, cnt = reduce(choose_texts, processedText, ([], processedText, 0, 0))#chosenTexts je lista karaktera
-    tokens, text, index = reduce(create_tokens, chosenTexts, ([], chosenTexts, 0))
+    returnTuple = reduce(choose_texts, processedText, ([], processedText, 0, 0))#chosenTexts je lista karaktera
+    chosenTexts = returnTuple[0]
 
-    #Ideja za 3. zad, 3. tacka: izmapirati niz tokena tako da dobijemo niz taplova, gde je prvi clan u taplu token, a drugi broj pojavaljivanja tog tapla(na pocetku za svaki token bice 1)
-    #sortirati niz taplova po tokenima tako da taplovi sa istim tokenima budu jedan do drugog u nizu
-    #reducovati niz taplova tako da se ne pojavaljuje vise tokena, nego da za svaki token imamo tacan broj pojavljivanja.
-    tupleTokens = pool.map(create_token_tuples, tokens)
-    sortedTupleTokens = sorted(tupleTokens, key=lambda tup: tup[0])
-    reducedTupleTokens, token, index = reduce(reduce_token_tuples, sortedTupleTokens, ([], 0, -1))
-    sortedReducedTupleTokens = sorted(reducedTupleTokens, key=lambda tup: tup[1], reverse=True)
-    print(sortedReducedTupleTokens)
+    currentListOfTokens = []# cemu bi ovo trebalo da sluzi?
+    for i in range(5000):
+        if i < 100 or i > 4900:
+            print(i, chosenTexts)
+        returnTuple = reduce(create_tokens, chosenTexts, ([], chosenTexts, 0))# returnTuple je samo promenljiva u koju pakujem povratnu vrednost reduce-a posto on vraca tuple.
+        listOfCandidatesForNextToken = returnTuple[0]                         # Uvek ce se na nultom indeksu nalaziti niz koji mi je dalje potreban; ostali elementi tuple-a su nebitni.
+        tupleTokens = pool.map(create_token_tuples, listOfCandidatesForNextToken)
+        sortedTupleTokens = sorted(tupleTokens, key=lambda tup: tup[0])
+        returnTuple = reduce(reduce_token_tuples, sortedTupleTokens, ([], 0, -1))
+        reducedTupleTokens = returnTuple[0]
+        sortedReducedTupleTokens = sorted(reducedTupleTokens, key=lambda tup: tup[1], reverse=True)
 
-# stao sam kod zadatka 3, 6. tacka
+        mostCommonToken = sortedReducedTupleTokens[0]
+        currentListOfTokens.append(mostCommonToken)
 
+        returnTuple = reduce(insert_token, chosenTexts, ([], mostCommonToken, "", []))
+        chosenTexts = returnTuple[0]
+    print(currentListOfTokens, len(currentListOfTokens))
+
+#zadatak 3, tacka 8
 
 
 
