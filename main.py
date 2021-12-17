9ba49cf77c7e080bab3799dcd493ab6256b5ea0e
Nikola Tadic <ntadic4419rn@raf.edu.rs>
2021-12-17 09:41:50 +0100

    removed old comments

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
 import multiprocessing.pool
 from functools import reduce
 from functools import partial
-from numba import jit
+#from numba import jit
 import wikipedia
 
-wikipedia.set_lang("en")  # staviti jezik da bude english, posto nam za srpski mesa cirilicu i latinicu
+wikipedia.set_lang("en")
 
 specialCharacters = ['=', '-', ',', '!', '?', '.', '$', '(', ')', '[', ']', ';', '{', '}', '@', '#', '%', '^', '&', '*',
                      '_', '+', '-', '/', '\\', '\'', '"', '<', '>', '|']
 # '''Dohvata tekstove zahtevanog broja stranica koje se pojavljuju kao
 #  rezultati pretrage za zadatu kljucnu rec'''
-def get_pages(query, results=10):  # staviti posle results = 50
+def get_pages(query, results=50):
     titles = wikipedia.search(query, results=results)
     pages = list()
     for title in titles:
         try:
-            page = wikipedia.page(
-                title)  # ovo su samo naslovi stranica, ako zelimo tekst sa stranice: text = page.content
-
-            text = page.content
+            page = wikipedia.page(title)  # ovo su samo naslovi stranica
+            text = page.content # ovo je tekst sa stranice
             data = (query, text)
-
             # data = (query, page) # ovde dodajemo samo naslove stranica
             pages.append(data)
         except:
@@ -35,13 +32,11 @@ def append_list(array, currentElement):
 def lower_case(character):
     return str.lower(character)
 
-
 def remove_special_chars(array, currentChar):
     if currentChar not in specialCharacters:
         return array + list(currentChar)
     else:
         return array
-
 
 def add_terminator(array, currentChar):
     if currentChar == " " or currentChar == "\n" or currentChar == "\t":
@@ -49,29 +44,25 @@ def add_terminator(array, currentChar):
     else:
         return array + list(currentChar)
 
-
 def reduce_text(args, currentChar):
     array, flag = args
     if flag:
         return (array, flag)
-    if len(array) >= 5000 and currentChar == "$":  # staviti posle na 10_000
+    if len(array) >= 10_000 and currentChar == "$":
         flag = True
     return (array + list(currentChar), flag)
-
 
 def prepare(args):
     query, string1 = args
     res = map(lower_case, str(string1))
     string2 = reduce(remove_special_chars, list(res), [])
-    string3 = reduce(add_terminator, string2,
-                     [])  # ukoliko imamo u stringu vise povezanih space-eva, stavice za svaki space $, da li je to validno?
+    string3 = reduce(add_terminator, string2, [])
     string3.append("$")
     string4, boolean = reduce(reduce_text, string3, ([], False))
     return query, string4
 
-
 def choose_texts(args,
-                 currentElement):  # uzima po pet tekstova za svaku kljucnu rec; za sada ne bira nasumicno, nego za svaku kljucnu rec prvih 5 tekstova!
+                 currentElement):  # uzima po pet tekstova za svaku kljucnu rec; ne bira nasumicno, nego za svaku kljucnu rec prvih 5 tekstova
     newArray, text, index, counter = args
     key, currentChar = currentElement
     previousKey, value = text[index - 1]
@@ -82,7 +73,6 @@ def choose_texts(args,
     else:
         return (newArray, text, index + 1, counter + 1)
 
-
 def create_tokens(args, currentChar):
     array, chosenTexts, index = args
     if index == len(chosenTexts) - 1 or currentChar == "$":
@@ -92,10 +82,8 @@ def create_tokens(args, currentChar):
     strings.append(token)
     return (array + strings, chosenTexts, index + 1)
 
-
 def create_token_tuples(currentToken):
     return (currentToken, 1)
-
 
 def reduce_token_tuples(args, currentTokenTuple):
     token, value = currentTokenTuple
@@ -111,14 +99,13 @@ def reduce_token_tuples(args, currentTokenTuple):
         copiedArray[index] = (key, int(num) + 1)
         return (copiedArray, token, index)
 
-
 def put_token(args, currentChar):
     newText, tokenTuple, originalText, index, sleep = args
     if (index < sleep):
         return (newText, tokenTuple, originalText, index + 1, sleep)
     token, value = tokenTuple
-    if (currentChar in token):  # ovde i
-        tmp = "".join(originalText[index: index + len(token)])  # ovde je bio problem
+    if (currentChar in token):
+        tmp = "".join(originalText[index: index + len(token)])
         tmp = tmp[0:len(token)]
         if (tmp == token):
             helpArray = []
@@ -167,10 +154,10 @@ if __name__ == '__main__':
     processedText = pool.map(prepare, titlesList)
     returnTuple = reduce(choose_texts, processedText, ([], processedText, 0, 0))  # chosenTexts je lista karaktera
     chosenTexts = returnTuple[0]
-    currentListOfTokens = []  # cemu bi ovo trebalo da sluzi?
-    for i in range(10):  # staviti posle broj iteracija na 5000
-        returnTuple = reduce(create_tokens, chosenTexts, ([], chosenTexts,0))  # returnTuple je samo promenljiva u koju pakujem povratnu vrednost reduce-a posto on vraca tuple.
-        listOfCandidatesForNextToken = returnTuple[0]  # Uvek ce se na nultom indeksu nalaziti niz koji mi je dalje potreban; ostali elementi tuple-a su nebitni.
+    currentListOfTokens = []  # lista koja pise u tekstu zadatka da je potrebna, ali se nigde ne koristi
+    for i in range(100):  # staviti posle broj iteracija na 5000
+        returnTuple = reduce(create_tokens, chosenTexts, ([], chosenTexts,0))  # returnTuple je samo promenljiva u koju pakujemo povratnu vrednost reduce-a posto on vraca tuple.
+        listOfCandidatesForNextToken = returnTuple[0]  # Uvek ce se na nultom indeksu nalaziti niz koji je dalje potreban; ostali elementi tuple-a su nebitni.
 
         tupleTokens = pool.map(create_token_tuples, listOfCandidatesForNextToken)
         sortedTupleTokens = sorted(tupleTokens, key=lambda tup: tup[0])
@@ -182,7 +169,7 @@ if __name__ == '__main__':
         currentListOfTokens.append(mostCommonToken)  # -> nije neophodno, ne koristi se nigde ta lista
 
         returnTuple = reduce(put_token, chosenTexts, ([], mostCommonToken, chosenTexts, 0, 0))
-        chosenTexts = returnTuple[0]  # dodaje buffer ukoliko nije prazan
+        chosenTexts = returnTuple[0]
 
     tupleTokens = pool.map(create_token_tuples, chosenTexts)
     sortedTupleTokens = sorted(tupleTokens, key=lambda tup: tup[0])
@@ -198,7 +185,7 @@ if __name__ == '__main__':
     # print(newText1)
 
     #tokeniziranje svih tekstova
-    newTexts = pool.map(partial(tokenize_all_texts, sortedReducedTupleTokens), processedText)# newTexts je lista taplova gde je key kljucna rec, a value je niz tokena(i karaktera, ali i samo jedan karakter sada posmatrama kao token)
+    newTexts = pool.map(partial(tokenize_all_texts, sortedReducedTupleTokens), processedText)# newTexts je lista taplova gde je key kljucna rec, a value je niz tokena(i karaktera, ali i samo jedan karakter sada posmatramo kao token)
 
     #brojanje koliko puta se odredjeni token pojavljuje u jednom tekstu
     # text1 = newTexts[0]
